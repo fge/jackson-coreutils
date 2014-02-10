a0d6b154f4eb6edc113cd815cc623c14ef9a987a
Francis Galiegue <fgaliegue@gmail.com>
2014-02-10 10:13:44 +0100

    dorelease.sh: remove obsolete comment

--- dorelease.sh
+++ dorelease.sh
@@ -3,8 +3,6 @@
 #
 # This will build everything that is needed and push to Maven central.
 #
-# The only thing missing is making this script non interactive...
-#
 
 ./gradlew --recompile-scripts clean test uploadArchives
 
