9743646d3bb018f66d38dcd7f4290d6fa2042cbc
Francis Galiegue <fgaliegue@gmail.com>
2014-03-10 23:29:42 +0100

    New JsonReader class
    This class aims to "fix" JSON parsing when the input contains trailing input,
    for instance:
    
        []]
    
    In this case, Jackson will happily read [], but ignore the trailing ]. This
    class will not ignore it given the appropriate constructor arguments and will
    throw an exception instead.

---
+++ src/main/java/com/github/fge/jackson/JsonReader.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of both licenses is available under the src/resources/ directory of
+ * this project (under the names LGPL-3.0.txt and ASL-2.0.txt respectively).
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.jackson;
+
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jackson.JacksonUtils;
+import com.google.common.io.Closer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.EnumSet;
+
+public final class JsonReader
+{
+    private static final EnumSet<JsonParser.Feature> DEFAULT_FEATURES;
+
+    static {
+        final EnumSet<JsonParser.Feature> set
+            = EnumSet.noneOf(JsonParser.Feature.class);
+        for (final JsonParser.Feature feature: JsonParser.Feature.values())
+            if (feature.enabledByDefault())
+                set.add(feature);
+        DEFAULT_FEATURES = EnumSet.copyOf(set);
+    }
+
+    private final JsonFactory factory;
+    private final boolean fullRead;
+
+    public JsonReader(final Collection<JsonParser.Feature> features,
+        final boolean fullRead)
+    {
+        this.fullRead = fullRead;
+        final ObjectMapper mapper = JacksonUtils.newMapper();
+        final EnumSet<JsonParser.Feature> set
+            = EnumSet.copyOf(DEFAULT_FEATURES);
+        set.addAll(features);
+
+        for (final JsonParser.Feature feature: set)
+            mapper.configure(feature, true);
+
+        factory = mapper.getFactory();
+    }
+
+    public JsonNode fromInputStream(final InputStream in)
+        throws IOException
+    {
+        final Closer closer = Closer.create();
+        final JsonParser parser;
+
+        try {
+            parser = closer.register(factory.createParser(in));
+            return readNode(parser);
+        } finally {
+            closer.close();
+        }
+    }
+
+    private JsonNode readNode(final JsonParser parser)
+        throws IOException
+    {
+        final JsonNode ret = parser.readValueAsTree();
+        if (!fullRead)
+            return ret;
+        final JsonLocation location = parser.getCurrentLocation();
+
+        try {
+            if (parser.nextToken() != null)
+                throw new IOException();
+        } catch (IOException e) {
+            throw new IOException(String.format("trailing input detected" +
+                " (last valid location: line %d, column %d)",
+                location.getLineNr(), location.getColumnNr()), e);
+        }
+        return ret;
+    }
+}
---
+++ src/test/java/com/github/fge/jackson/JsonReaderTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of both licenses is available under the src/resources/ directory of
+ * this project (under the names LGPL-3.0.txt and ASL-2.0.txt respectively).
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import java.util.EnumSet;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+public final class JsonReaderTest
+{
+    @Test
+    public void streamIsClosedOnRead()
+        throws IOException
+    {
+        final InputStream in = spy(stringToInputStream("[]"));
+        new JsonReader(EnumSet.noneOf(JsonParser.Feature.class), false)
+            .fromInputStream(in);
+        verify(in).close();
+    }
+
+    @Test
+    public void parsingOptionsAreRespected()
+        throws IOException
+    {
+        final Collection<JsonParser.Feature> features
+            = EnumSet.of(JsonParser.Feature.ALLOW_SINGLE_QUOTES);
+        final JsonReader reader = new JsonReader(features, false);
+        final InputStream in = spy(stringToInputStream("'hello'"));
+        assertEquals(reader.fromInputStream(in),
+            JacksonUtils.nodeFactory().textNode("hello"));
+    }
+
+    @Test
+    public void noFullReadDoesNotThrowExceptionOnExtraInput()
+        throws IOException
+    {
+        final JsonReader reader
+            = new JsonReader(EnumSet.noneOf(JsonParser.Feature.class), false);
+        reader.fromInputStream(stringToInputStream("[]]"));
+        assertTrue(true);
+    }
+
+    @Test
+    public void fullReadThrowsExceptionOnExtraInput()
+        throws UnsupportedEncodingException
+    {
+        final JsonReader reader
+            = new JsonReader(EnumSet.noneOf(JsonParser.Feature.class), true);
+        final InputStream in = stringToInputStream("[]]");
+        try {
+            reader.fromInputStream(in);
+            fail("No exception thrown!");
+        } catch (IOException e) {
+            assertTrue(e.getMessage().startsWith("trailing input detected"));
+        }
+        assertTrue(true);
+
+    }
+
+    private static InputStream stringToInputStream(final String input)
+        throws UnsupportedEncodingException
+    {
+        return new ByteArrayInputStream(input.getBytes("UTF-8"));
+    }
+}
