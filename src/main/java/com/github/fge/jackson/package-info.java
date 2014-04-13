3c851f145419228de1d9fb9342df5e2bb251b13d
Francis Galiegue <fgaliegue@gmail.com>
2014-04-13 20:24:01 +0200

    Javadoc updates

--- src/main/java/com/github/fge/jackson/package-info.java
+++ src/main/java/com/github/fge/jackson/package-info.java
@@ -22,11 +22,10 @@
  *
  * <p>{@link com.github.fge.jackson.JsonLoader} contains various methods to load
  * JSON documents as {@link com.fasterxml.jackson.databind.JsonNode}s. It uses
- * a {@link com.github.fge.jackson.JsonNodeReader} with full read enabled (as
- * such, parsing {@code []]} will generate an error where Jackson normally does
- * not).</p>
+ * a {@link com.github.fge.jackson.JsonNodeReader} (as such, parsing {@code []]}
+ * will generate an error where Jackson normally does not).</p>
  *
- * <p></p>You will also want to use {@link com.github.fge.jackson.JacksonUtils}
+ * <p>You will also want to use {@link com.github.fge.jackson.JacksonUtils}
  * to grab a node factory, reader and pretty printer for anything JSON. Compared
  * to the basic Jackson's {@link com.fasterxml.jackson.databind.ObjectMapper},
  * the one provided by {@link com.github.fge.jackson.JacksonUtils} deserializes
@@ -41,8 +40,9 @@
  * number values.</p>
  *
  * <p>Finally, {@link com.github.fge.jackson.NodeType} is a utility enumeration
- * which distinguishes between all JSON node types defined by RFC 4627, plus
+ * which distinguishes between all JSON node types defined by RFC 7159, plus
  * {@code integer} (used by JSON Schema). Note that since Jackson 2.2, there is
- * also {@link com.fasterxml.jackson.databind.JsonNode#getNodeType()}.</p>
+ * also {@link com.fasterxml.jackson.databind.JsonNode#getNodeType()}, but it
+ * does not make a difference between {@code number} and {@code integer}.</p>
  */
 package com.github.fge.jackson;
